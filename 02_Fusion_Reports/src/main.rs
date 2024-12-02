524430943c65d488d0bd2b1b89e1e510766af9a4
John Forster <john.forster@makers.tech>
2024-12-02 15:30:29 +0000

    Refactor Day 2

--- 02_Fusion_Reports/src/main.rs
+++ 02_Fusion_Reports/src/main.rs
@@ -1,4 +1,3 @@
-use colored::{ColoredString, Colorize};
 use std::fs;
 
 #[derive(PartialEq, Clone, Copy, Debug)]
@@ -7,10 +6,11 @@ enum Direction {
     Decreasing,
 }
 
-type Level = i32;
+type Level = u32;
 
 const MAX_DIFFERENCE: Level = 3;
 const MIN_DIFFERENCE: Level = 1;
+
 fn main() {
     let input_string = fs::read_to_string("./input.txt").unwrap();
 
@@ -23,54 +23,67 @@ fn check_input(input_string: &str) -> i32 {
     let mut safe_count = 0;
 
     for line in input_string.trim().split("\n") {
-        let mut safe = check_report(line);
-        if !safe {
-            let levels: Vec<&str> = line.trim().split(" ").collect();
-            let length = levels.len();
-            'level: for i in 0..length {
-                let mut new_levels = levels.clone();
-                new_levels.remove(i);
-                let new_safe = check_report(&new_levels.join(" "));
-
-                if new_safe {
-                    safe = true;
-                    break 'level;
-                }
-            }
-        }
-
+        let safe = check_report(line);
         if safe {
-            safe_count += 1;
+            safe_count += 1
         }
     }
 
     safe_count
 }
 
-fn check_report(line: &str) -> bool {
+fn check_report(report_string: &str) -> bool {
+    let report: Vec<Level> = report_string
+        .trim()
+        .split(" ")
+        .map(|x| x.parse::<Level>().unwrap())
+        .collect();
+
+    let length = report.len();
+
+    for i in 0..length {
+        let filtered_report = report
+            .iter()
+            .enumerate()
+            .filter(|(j, _)| i != *j)
+            .map(|(_, x)| x);
+
+        let safe = report_is_valid(filtered_report);
+        if safe {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn report_is_valid<'a, I>(levels: I) -> bool
+where
+    I: Iterator<Item = &'a Level>,
+{
     let mut prev_level: Option<Level> = None;
     let mut direction: Option<Direction> = None;
-
-    for level in line.split(" ").map(|x| x.parse::<Level>().unwrap()) {
+    for &level in levels {
         match prev_level {
             None => {
                 prev_level = Some(level);
             }
             Some(prev) => {
-                let difference = level - prev;
-                let new_direction = match difference.is_positive() {
-                    true => Direction::Decreasing,
-                    false => Direction::Increasing,
+                let diff = level.abs_diff(prev);
+                if diff > MAX_DIFFERENCE || diff < MIN_DIFFERENCE {
+                    return false;
+                }
+
+                let new_direction = match level > prev {
+                    true => Some(Direction::Decreasing),
+                    false => Some(Direction::Increasing),
                 };
 
                 if direction.is_none() {
-                    direction = Some(new_direction);
+                    direction = new_direction;
                 }
 
-                if difference.abs() > MAX_DIFFERENCE
-                    || difference.abs() < MIN_DIFFERENCE
-                    || direction.unwrap() != new_direction
-                {
+                if direction != new_direction {
                     return false;
                 } else {
                     prev_level = Some(level)
